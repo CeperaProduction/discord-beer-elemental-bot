c12a208120d7c38b3d19dbbc1bf93b6165a69bc6
Cepera <Cepera@CeperaPCn2>
2023-08-07 10:37:50 +0300

    Cleanup dependencies

--- src/main/java/me/cepera/discord/bot/beerelemental/Application.java
+++ src/main/java/me/cepera/discord/bot/beerelemental/Application.java
@@ -1,12 +1,10 @@
 package me.cepera.discord.bot.beerelemental;
 
-import org.apache.commons.cli.ParseException;
-
 import me.cepera.discord.bot.beerelemental.di.DaggerBaseComponent;
 
 public class Application {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
 
         DaggerBaseComponent.create().getDiscordBot().start();
 
